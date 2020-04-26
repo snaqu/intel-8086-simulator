1992728e2844afc700a8601e050ded6589b366b4
pablomiklas@gmail.com <pablomiklas@gmail.com>
2020-04-26 20:42:44 +0200

    Updates

---
+++ precache-manifest.af12710f7d019ab6f7c912781e2ba40a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b81785d24e1eeb4f2844d2986aa51f36",
+    "url": "/intel-8086-simulator/index.html"
+  },
+  {
+    "revision": "0006c79c3cd71210dbc3",
+    "url": "/intel-8086-simulator/static/css/main.a5593a05.chunk.css"
+  },
+  {
+    "revision": "2514e26494ac102b05e5",
+    "url": "/intel-8086-simulator/static/js/2.56036511.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/intel-8086-simulator/static/js/2.56036511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0006c79c3cd71210dbc3",
+    "url": "/intel-8086-simulator/static/js/main.855c4ebb.chunk.js"
+  },
+  {
+    "revision": "1254c3a80fb350511813",
+    "url": "/intel-8086-simulator/static/js/runtime-main.470bcc90.js"
+  }
+]);
